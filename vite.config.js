4d4f873cda0e880526150a55d1d5b21fd6241ec8
elisabettafedele <efedele@ethz.ch>
2025-07-10 19:12:26 -0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/', // ✅ Set correct base path for custom domain
   plugins: [react()],
 })
